282176cfa8c62195ee9d7596a99ccd1a3fb16e85
Malcolm Liljedahl <malcolm.liljedahl94@gmail.com>
2022-02-10 15:32:36 +0100

    task1 done

--- task1/tcpclient/TCPClient.java
+++ task1/tcpclient/TCPClient.java
@@ -1,18 +1,34 @@
 package tcpclient;
-import java.net.*;
+import java.net.*; //Socket class
 import java.io.*;
 
 public class TCPClient {
-    private static int STATIC_BUFFER_SIZE=2048;
+    private static int STATIC_BUFFER_SIZE=1024;
 
-    public TCPClient() {
-    }
+    public TCPClient() {}
 
     public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
-        byte[] fromUserBuffer = new byte[STATIC_BUFFER_SIZE]; 
+
+        /*Arrays that stores the information we send and recive*/
+        byte[] fromUserBuffer = new byte[0]; 
         byte[] fromServerBuffer = new byte[STATIC_BUFFER_SIZE];
-        Socket clientSocket = new Socket(hostname, port); 
+        ByteArrayOutputStream bs = new ByteArrayOutputStream(); //dynamic array
+
+        /*Creates a socket for communication*/
+        Socket clientSocket = new Socket(hostname, port);
+
+        InputStream inputStream = clientSocket.getInputStream(); //reads the input data
+        OutputStream outputStream = clientSocket.getOutputStream(); // writes the output data
+        outputStream.write(toServerBytes); //sends bytes on socket
         
-        return new byte[0];
+        int fromUserLength = 0; // counts how many bytes we have read.
+        do {
+            fromUserLength = inputStream.read(fromServerBuffer); //reads the data
+            bs.write(fromServerBuffer, 0, fromUserLength);
+            fromUserBuffer = bs.toByteArray();
+        } while(inputStream.read() != -1);
+
+        clientSocket.close();
+        return fromUserBuffer;
     }
 }
